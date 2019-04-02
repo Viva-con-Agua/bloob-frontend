9e39b4dd5392c6c496e875876727e428180a1b83
Tom Bierschenk <bierscht@hu-berlin.de>
2019-04-02 12:26:50 +0200

    pool network integration

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  baseUrl: process.env.NODE_ENV === "production" ? "/bloob/" : "/bloob/",
+  chainWebpack: config => {
+    const svgRule = config.module.rule("svg");
+
+    // clear all existing loaders.
+    // if you don't do this, the loader below will be appended to
+    // existing loaders of the rule.
+    svgRule.uses.clear();
+
+    // add replacement loader(s)
+    svgRule.use("svg-inline-loader").loader("svg-inline-loader");
+  }
+};
