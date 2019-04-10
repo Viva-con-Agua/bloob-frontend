dc42d26b19dda81c8d0413d7829426b7e862279c
Tom Bierschenk <bierscht@hu-berlin.de>
2019-04-10 15:12:19 +0200

    vuex added state and mutations

--- src/store.js
+++ src/store.js
@@ -4,7 +4,32 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    reciever: [],
+    sender_name: "",
+    sender_mail: "",
+    subject: "",
+    message: ""
+  },
+  mutations: {
+    AddReciever(state, n) {
+      state.reciever.push(n);
+    },
+    SetReciever(state, n) {
+      state.reciever = n;
+    },
+    sender_name(state, n) {
+      state.sender_name = n;
+    },
+    sender_mail(state, n) {
+      state.sender_name = n;
+    },
+    subject(state, n) {
+      state.subject = n;
+    },
+    message(state, n) {
+      state.message = n;
+    }
+  },
   actions: {}
 });
