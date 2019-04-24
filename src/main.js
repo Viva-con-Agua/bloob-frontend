d1d7242f6724d2cceac7cfae2b7024c54f1ea4f3
Tom Bierschenk <bierscht@hu-berlin.de>
2019-04-24 09:28:56 +0200

    get user from backend and save it in vuex

--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
-import ElementUI from 'element-ui';
-import { WidgetUserAutocomplete } from 'vca-widget-user'
-import { WidgetTopNavigation, WidgetBottomNavigation } from 'vca-widget-navigation'
-import router from './router'
-import VueI18n from 'vue-i18n'
-import en from '@/lang/en_US.json';
-import de from '@/lang/de_DE.json';
-import enElement from 'element-ui/lib/locale/lang/en';
-import deElement from 'element-ui/lib/locale/lang/de';
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import ElementUI from "element-ui";
+import { WidgetUserAutocomplete } from "vca-widget-user";
+import {
+  WidgetTopNavigation,
+  WidgetBottomNavigation
+} from "vca-widget-navigation";
+import router from "./router";
+import VueI18n from "vue-i18n";
+import en from "@/lang/en_US.json";
+import de from "@/lang/de_DE.json";
+import enElement from "element-ui/lib/locale/lang/en";
+import deElement from "element-ui/lib/locale/lang/de";
+import store from "./store";
 import BootstrapVue from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
@@ -18,27 +21,39 @@ import "vue-wysiwyg/dist/vueWysiwyg.css";
 
 Vue.use(wysiwyg, {}); // config is optional
 
-
 const dateTimeFormats = {
-  'en-US': {
+  "en-US": {
     short: {
-      year: 'numeric', month: 'short', day: 'numeric'
+      year: "numeric",
+      month: "short",
+      day: "numeric"
     },
     long: {
-      year: 'numeric', month: 'short', day: 'numeric',
-      weekday: 'short', hour: 'numeric', minute: 'numeric'
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+      weekday: "short",
+      hour: "numeric",
+      minute: "numeric"
     }
   },
-  'de-DE': {
+  "de-DE": {
     short: {
-      year: 'numeric', month: 'short', day: 'numeric'
+      year: "numeric",
+      month: "short",
+      day: "numeric"
     },
     long: {
-      year: 'numeric', month: 'short', day: 'numeric',
-      weekday: 'short', hour: 'numeric', minute: 'numeric', hour12: true
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+      weekday: "short",
+      hour: "numeric",
+      minute: "numeric",
+      hour12: true
     }
   }
-}
+};
 
 Vue.use(VueI18n);
 
@@ -47,22 +62,29 @@ const i18n = new VueI18n({
   locale: locale,
   dateTimeFormats,
   messages: {
-    'en-US': Object.assign(en, enElement),
-    'de-DE': Object.assign(de, deElement),
-    'de': Object.assign(de, deElement),
-    'en': Object.assign(en, enElement)
+    "en-US": Object.assign(en, enElement),
+    "de-DE": Object.assign(de, deElement),
+    de: Object.assign(de, deElement),
+    en: Object.assign(en, enElement)
   }
 });
 
 Vue.use(ElementUI, {
   i18n: (key, value) => i18n.t(key, value)
 });
-Vue.use(WidgetUserAutocomplete, { 'i18n': i18n })
-Vue.use(WidgetBottomNavigation, { 'i18n': i18n })
-Vue.use(WidgetTopNavigation, { 'i18n': i18n })
+Vue.use(WidgetUserAutocomplete, { i18n: i18n });
+Vue.use(WidgetBottomNavigation, { i18n: i18n });
+Vue.use(WidgetTopNavigation, { i18n: i18n });
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
+
+// only for init - checks if there is already a user, initiates OAuth handshake otherwise
+var u = store.getters["user/get"];
+if (u === null) {
+  // Handshake!
+  store.dispatch("user/init").then(() => {});
+}
 
 new Vue({
   router,
@@ -75,4 +97,4 @@ new Vue({
   },
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
