deddf8cd99561979d679b0b901917a8d90dbb705
Mario Völker <voelker.mario@gmail.com>
2019-05-04 10:44:38 +0800

    Code drop for i18n an. String from Compose form partially externalised

--- src/views/Compose.vue
+++ src/views/Compose.vue
@@ -3,39 +3,74 @@
     <router-view>
       <!-- here the MosaicoModal component will be rendered -->
     </router-view>
-      <div class="mt-2">Empfänger: {{ involvedSupporter }}</div>
+    <div class="mt-2">
+      {{ $t("compose.form.label.recipients") }} {{ involvedSupporter }}
+    </div>
 
-<!-- TODO: import user search from Drops and save selected recievers
-      -> WidgetUserAutocomplete can search in drops user data
-      -> Problem i need the right event to recieve a selected user
-        @vca-use-selection does not trigger, am i missing something?
-      trying multiple fields and event names, searching for the right one
--->
+    <!-- TODO: import user search from Drops and save selected recievers
+              -> WidgetUserAutocomplete can search in drops user data
+              -> Problem i need the right event to recieve a selected user
+                @vca-use-selection does not trigger, am i missing something?
+              trying multiple fields and event names, searching for the right one
+        -->
     <WidgetUserAutocomplete
-      :placeholder="$t('donation.placeholder.involved.indicator')"
+      :placeholder="$t('compose.form.placeholder.recipients')"
       :preselection="involvedSupporter"
       :SelectableUser="recipients"
       @vca-user-selection="addRecipient"
     />
 
-    <b-form @submit.prevent="onSubmit" @reset="doResetMessageStateToDefault" v-if="show">
+    <b-form
+      @submit.prevent="onSubmit"
+      @reset="doResetMessageStateToDefault"
+      v-if="show"
+    >
       <!-- present sender name selection -->
-      <b-form-group id="sender-name-group" label="Senden als:" label-for="senderName">
+      <b-form-group
+        id="sender-name-group"
+        :label="$t('compose.form.label.fromName')"
+        label-for="senderName"
+      >
         <b-form-select id="sender-name" required v-model="senderName">
-          <option :value="null">Select one</option>
+          <option :value="null">{{
+            $t("compose.form.placeholder.fromName")
+          }}</option>
           <option v-for="role in roles" :key="role.name" :value="role.name">
-            {{role.name}}
+            {{ role.name }}
           </option>
         </b-form-select>
       </b-form-group>
 
       <!-- present sender mail selection -->
-      <b-form-group id="sender-mail-group" label="Senden von:" label-for="senderMail">
-        <b-form-select id="sender-mail" :options="senderMailTestdata" required v-model="senderMail" />
+      <b-form-group
+        id="sender-mail-group"
+        :label="$t('compose.form.label.fromMail')"
+        label-for="senderMail"
+      >
+        <b-form-select
+          id="sender-mail"
+          required
+          v-model="senderMail"
+        >
+          <option :value="null">{{
+            $t("compose.form.placeholder.fromMail")
+          }}</option>
+          <option
+            v-for="testsender in senderMailTestdata"
+            :key="testsender.value"
+            :value="testsender.text"
+          >
+            {{ testsender.text }}
+          </option>
+        </b-form-select>
       </b-form-group>
 
-<!--      &lt;!&ndash; subject line &ndash;&gt;-->
-      <b-form-group id="subject-line-group" label="Betreff:" label-for="subjectLine">
+      <!--      &lt;!&ndash; subject line &ndash;&gt;-->
+      <b-form-group
+        id="subject-line-group"
+        :label="$t('compose.form.label.subject')"
+        label-for="subjectLine"
+      >
         <b-form-input
           id="subject-line"
           type="text"
@@ -45,13 +80,13 @@
         />
       </b-form-group>
 
-<!--      &lt;!&ndash; 2 example checkmarks &ndash;&gt;-->
-<!--      <b-form-group id="exampleGroup4">-->
-<!--        <b-form-checkbox-group v-model="hecked" id="exampleChecks">-->
-<!--          <b-form-checkbox value="me">Check me out</b-form-checkbox>-->
-<!--          <b-form-checkbox value="that">Check that out</b-form-checkbox>-->
-<!--        </b-form-checkbox-group>-->
-<!--      </b-form-group>-->
+      <!--      &lt;!&ndash; 2 example checkmarks &ndash;&gt;-->
+      <!--      <b-form-group id="exampleGroup4">-->
+      <!--        <b-form-checkbox-group v-model="hecked" id="exampleChecks">-->
+      <!--          <b-form-checkbox value="me">Check me out</b-form-checkbox>-->
+      <!--          <b-form-checkbox value="that">Check that out</b-form-checkbox>-->
+      <!--        </b-form-checkbox-group>-->
+      <!--      </b-form-group>-->
 
       <div id="mail-body">
         <mosaico></mosaico>
@@ -63,7 +98,6 @@
 </template>
 
 <script>
-
 //import { FormItem, Select, Option } from 'element-ui'
 import { WidgetUserAutocomplete } from "vca-widget-user";
 import Mosaico from "@/components/Mosaico.vue"; // @ is an alias to /src
@@ -86,7 +120,14 @@ export default {
   data() {
     return {
       involvedSupporter: [],
-      senderMailTestdata: [{ text: 'Select One', value: null }, 'noreply@vivaconagua.org', 'berlin@vivaconagua.org', 'maxmusterman@vivaconagua.org'],
+      senderMailTestdata: [
+        {
+          text: "noreply@vivaconagua.org",
+          value: 1
+        },
+        { text: "berlin@vivaconagua.org", value: 2 },
+        { text: "maxmusterman@vivaconagua.org", value: 3 }
+      ],
       // TODO: get user name and role from Drops
       // userRoles: [{ text: 'Select One', value: null }, 'noreply@vivaconagua.org', 'berlin@vivaconagua.org', 'maxmusterman@vivaconagua.org'],
       // TODO: get user role from Drops and check access rights to mail adresses
@@ -97,13 +138,13 @@ export default {
     ...mapFields(messageStore, {
       subject: "subject",
       senderName: "senderName",
-      senderMail: "senderMail",
+      senderMail: "senderMail"
     }),
     ...mapFields(userStore, {
       roles: "user.roles"
     }),
     ...mapMultiRowFields(messageStore, {
-      recipients : "recipients"
+      recipients: "recipients"
     })
   },
   methods: {
@@ -117,7 +158,6 @@ export default {
       alert("Submitted. Reset state to default.");
       this.doResetMessageStateToDefault();
     }
-  },
-}
-
+  }
+};
 </script>
